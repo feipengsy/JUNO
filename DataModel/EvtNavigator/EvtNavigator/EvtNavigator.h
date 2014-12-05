ebbd15a9b3b99dace769752cb5ead8be956cea4c
LiTeng <liteng_shiyan@163.com>
2014-12-05 10:31:52 +0800

    import EvtNavigator

---
+++ DataModel/EvtNavigator/EvtNavigator/EvtNavigator.h
@@ -0,0 +1,57 @@
+#ifndef EVTNAVIGATOR_H
+#define EVTNAVIGATOR_H
+
+#include "TObject.h"
+#include "TClass.h"
+#include "TTimeStamp.h"
+#include "RootIOUtil/SmartRef.h"
+#include "Event/HeaderObject.h"
+#include <vector>
+#include <string>
+
+namespace JM {
+
+class EvtNavigator : public TObject {
+
+public:
+
+    EvtNavigator() { Class()->IgnoreTObjectStreamer(); }
+
+    ~EvtNavigator();
+
+    JM::HeaderObject* getHeader(const std::string& path);
+
+    void addHeader(const std::string& path, JM::HeaderObject* header);
+
+    void setHeaderEntry(const std::string& path, int entry);
+
+    std::vector<std::string>& getPath();
+
+    void setPath(const std::vector<std::string>& paths);
+
+    void setWriteFlag(const std::string& path, bool write = false);
+
+    bool writePath(const std::string& path);
+
+    void resetWriteFlag();
+
+    const TTimeStamp& TimeStamp() const;
+
+    TTimeStamp& TimeStamp();
+
+    void setTimeStamp(const TTimeStamp& value);
+    
+private:
+    // paths and write flags won't be saved
+    std::vector<std::string> m_paths; //!
+    std::vector<bool> m_writeFlag; //!
+    std::vector<JM::SmartRef*> m_refs;
+    TTimeStamp m_TimeStamp; // Time stamp of event
+
+ClassDef(EvtNavigator,1)
+
+};
+
+} // namespace JM
+
+#endif
---
+++ DataModel/EvtNavigator/src/EvtNavigator.cc
@@ -0,0 +1,84 @@
+#include "EvtNavigator/EvtNavigator.h"
+#include <algorithm>
+
+ClassImp(JM::EvtNavigator);
+
+JM::EvtNavigator::~EvtNavigator()
+{
+    std::vector<JM::SmartRef*>::iterator it, end = m_refs.end();
+    for (it = m_refs.begin(); it != end; ++it) {
+        delete *it;
+    }
+}
+
+JM::HeaderObject* JM::EvtNavigator::getHeader(const std::string& path)
+{
+    std::vector<std::string>::iterator pos = find(m_paths.begin(), m_paths.end(), path);
+    if (m_paths.end() == pos) return 0;
+    return static_cast<JM::HeaderObject*>(m_refs[pos - m_paths.begin()]->GetObject());
+}
+
+std::vector<std::string>& JM::EvtNavigator::getPath()
+{
+    return m_paths;
+}
+
+void JM::EvtNavigator::setHeaderEntry(const std::string& path, int entry)
+{
+    std::vector<std::string>::iterator pos = find(m_paths.begin(), m_paths.end(), path);
+    if (m_paths.end() == pos) return;
+    m_refs[pos - m_paths.begin()]->setEntry(entry);
+}
+
+void JM::EvtNavigator::addHeader(const std::string& path, JM::HeaderObject* header)
+{
+    m_paths.push_back(path);
+    SmartRef* ref = new SmartRef();
+    m_refs.push_back(ref);
+    m_refs.back() -> SetObject(header);
+    m_writeFlag.push_back(true);
+}
+
+void JM::EvtNavigator::setPath(const std::vector<std::string>& paths)
+{
+    m_paths = paths;
+}
+
+bool JM::EvtNavigator::writePath(const std::string& path)
+{
+    std::vector<std::string>::iterator pos = find(m_paths.begin(), m_paths.end(), path);
+    if (m_paths.end() == pos) return false;
+    return m_writeFlag[pos - m_paths.begin()];
+}
+
+void JM::EvtNavigator::setWriteFlag(const std::string& path, bool write)
+{
+    std::vector<std::string>::iterator pos = find(m_paths.begin(), m_paths.end(), path);
+    if (m_paths.end() == pos) return;
+    m_writeFlag[pos - m_paths.begin()] = write;
+}
+
+void JM::EvtNavigator::resetWriteFlag()
+{
+    if (m_writeFlag.size()) m_writeFlag.clear();
+    int npath = m_paths.size();
+    for (int i = 0; i < npath; ++i) {
+        m_writeFlag.push_back(true);
+    }
+}
+
+const TTimeStamp& JM::EvtNavigator::TimeStamp() const
+{
+  return m_TimeStamp;
+}
+
+TTimeStamp& JM::EvtNavigator::TimeStamp()
+{
+  return m_TimeStamp;
+}
+
+void JM::EvtNavigator::setTimeStamp(const TTimeStamp& value)
+{
+  m_TimeStamp = value;
+}
+
